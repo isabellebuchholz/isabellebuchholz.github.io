31d487554814643cb02945945bd9884774084060
Isabelle <buchholz.isabelle@gmail.com>
2016-11-17 01:20:49 +0000

    added borders on billypedia

--- projects/billypedia/index.js
+++ projects/billypedia/index.js
@@ -2,9 +2,14 @@
 $(document).ready(function() {
     $.getJSON('data.json', function (data) {
         // YOUR CODE BELOW HERE //
+        $('#section-quotes').css('background-color', 'grey').css('border-radius', '1px');
+        $('#section-top-rated').css('background-color', 'grey').css('border-radius', '1px');
+        $('#section-bio').css('background-color', 'grey').css('border-radius', '1px');
+        $('#section-praise').css('background-color', 'grey').css('border-radius', '1px');
+        $('#Discography').css('background-color', 'grey').css('border-radius', '1px');
         
         // uncomment this to inspect all available data; delete when done //
-        // console.log(data);
+         console.log(data);
         
         // EXAMPLE: Looping over top rated recordings; replace with your code //
         let topRated = data.discography.topRated;
